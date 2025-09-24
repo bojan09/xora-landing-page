247397e21a4fe7f79f15545e6492112317d2a9e3
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-09-24 22:29:02 +0200

    header, commit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+import { clsx } from "clsx";
 
 const NavLink = ({ title }) => (
   <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-700 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
@@ -19,7 +20,12 @@ const Header = () => {
           <img src="/images/xora.svg" alt="logo" width={115} height={55} />
         </a>
         {/* Navbar container */}
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             {/* Navbar */}
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
@@ -30,7 +36,15 @@ const Header = () => {
                   <NavLink title="pricing" />
                 </li>
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
@@ -47,6 +61,24 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
+            {/* Mobile Sidebar Background */}
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="mobile-menu-bg"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="mobile-menu-bg"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
         {/* Hamburger Button */}
@@ -57,6 +89,7 @@ const Header = () => {
           <img
             src={`/images/${isOpen ? "close" : "magic"}.svg`}
             alt="hamburger menu"
+            className="size-1/2 object-contain"
           />
         </button>
       </div>
