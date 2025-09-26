1f6799b01cbc17f9e06bbc6bf39ec09f6b799369
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-09-26 23:00:42 +0200

    faq - commit

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { Element } from "react-scroll";
+import { faq } from "../constants";
+import { FaqItem } from "../components";
 
 const Faq = () => {
-  return <div></div>;
+  const halfLength = Math.floor(faq.length / 2);
+
+  return (
+    <section>
+      <Element name="faq" className="relative">
+        <div className="container relative z-2 py-28">
+          {/* Heading and Padding Wrapper */}
+          <div>
+            <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
+              Curiosity didn't kill the cat. It gave it answers.
+            </h3>
+            <p className="body-1 max-lg:max-w-sm">
+              You've got questions, we got answers
+            </p>
+          </div>
+
+          {/* Divider */}
+          <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2" />
+        </div>
+
+        {/* Wrapper for Q & A's */}
+        <div className="faq-glow_before relative z-2 border-2 border-s2 bg-s1">
+          <div className="container flex gap-10 max-lg:block">
+            <div className="rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
+              <img src="/images/faq-logo.svg" alt="logo" className="size-1/2" />
+            </div>
+            <div className="relative flex-1 pt-24">
+              {faq
+                .slice(0, halfLength)
+                .map(({ question, id, answer }, index) => (
+                  <FaqItem
+                    key={id}
+                    id={id}
+                    question={question}
+                    answer={answer}
+                    index={index}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
 };
 
 export default Faq;
