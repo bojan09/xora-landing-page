fc8fc5f4a37081d463c57c898ce4d509fcb535c2
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-09-27 20:28:36 +0200

    testimonials commit

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import clsx from "clsx";
 
-const TestimonialItem = () => {
-  return <div>TestimonialItem</div>;
+const TestimonialItem = ({
+  id,
+  name,
+  role,
+  avatarUrl,
+  comment,
+  containerClassName,
+}) => {
+  return (
+    <div
+      className={clsx(
+        "relative px-14 pt-11 pb-14 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md:-right-4",
+        containerClassName
+      )}
+    >
+      <blockquote className="h7 mb-8 text-p4">{comment}</blockquote>
+      <div className="flex items-center max-xl:-mr-8">
+        {/* Image container */}
+        <div className="mr-4 size-20 shrink-0 rounded-half border-2 border-s2 p-1.5">
+          <img src={avatarUrl} alt={name} className="size-full object-cover" />
+        </div>
+        {/* Text wrapper */}
+        <div className="">
+          <h4 className="body-2 mb-0.5 text-p1">{name}</h4>
+          <p className="small-compact uppercase text-s3">{role}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialItem;
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { TestimonialItem } from "../components";
+import { testimonials } from "../constants";
 
 const Testimonials = () => {
-  return <div></div>;
+  const halfLength = Math.floor(testimonials.length / 2);
+
+  return (
+    <section className="relative z-2 py-24 md:py-28 lg:py-40">
+      {/* Container */}
+      <div className="container block lg:flex">
+        {/* Heading container */}
+        <div className="testimonials_head-res relative   z-2 mr-20 flex-300 ">
+          <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
+          <h3 className="h3 max-md:h5 text-p4">Word from our fans</h3>
+        </div>
+        {/* Testimonials Container */}
+        <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block">
+          <div className="testimonials_group-after flex-50">
+            {testimonials
+              .slice(0, halfLength)
+              .map(({ id, name, role, avatarUrl, comment }) => (
+                <TestimonialItem
+                  key={id}
+                  id={id}
+                  name={name}
+                  role={role}
+                  avatarUrl={avatarUrl}
+                  comment={comment}
+                  containerClassName="last:after:hidden last:after:max-md:block"
+                />
+              ))}
+          </div>
+
+          <div className="flex-50">
+            {testimonials
+              .slice(halfLength)
+              .map(({ id, name, role, avatarUrl, comment }) => (
+                <TestimonialItem
+                  key={id}
+                  id={id}
+                  name={name}
+                  role={role}
+                  avatarUrl={avatarUrl}
+                  comment={comment}
+                  containerClassName="last:after:hidden after:right-auto after:left-0 after:max-md:-left-4 md:px-12"
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
